79bad36aeb4de982b261932b119b9542f14b79bd
1111 <nigelkys@gmail.com>
2022-07-06 17:44:21 -0700

    cmds.lua
    Commands

---
+++ cmds.lua
@@ -0,0 +1,17 @@
+Servers:
+  'autoreactions'
+  reaction[rt] add :emoji: <text> (optional: --strict on) @ Reacts to message. If you have straight on then it must exactly be that text
+  reaction[rt] list @ View all the reactions in server
+  reaction[rt] remove :emoji: <text> or autoreaciton remove select:2 @ Remove the autoreaction from server.
+  reaction[rt] author (member) @ View at all the auto reaction responses that member made
+  reaction[rt] removeall @ Remove all the reactions inside server
+  reaction[rt] clear <trigger> @ Remove all the reactions inside server that include that trigger
+  
+  'autoresponder'
+  autoresponder add <trigger>, <response> (optional: --strict on, --delete on, --self_destruct 3) @ Auto responder to a message
+  autoresponder list @ View all the autoresponders made inside server
+  
+Last.fm:
+  lastfm customreactions <left_emoji> <right_emoji> @ Display reactions on embed
+  lastfm whoknows (artist) @ Check who knows that artist inside the server
+  
